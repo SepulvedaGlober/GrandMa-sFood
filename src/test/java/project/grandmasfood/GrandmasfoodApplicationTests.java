78c6a5ddb3ed581e32168a976ac7c8dfc2d0218c
s.sepulveda01 <s.sepulveda01@globant.com>
2025-03-06 18:37:58 -0500

    Creation of the project

---
+++ src/test/java/project/grandmasfood/GrandmasfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package project.grandmasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandmasfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
